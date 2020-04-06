0c7776781c7fcaafdd770b85e57dea91c1a50832
Debayan Banerjee <debayanin@gmail.com>
2020-04-06 14:52:49 +0200

    Corrected webqsp

--- webqsp/tonif.py
+++ webqsp/tonif.py
@@ -2,7 +2,7 @@ from pynif import NIFCollection
 import sys,os,json,re
 
 
-d = json.loads(open('input/webqsp.train.entities.json').read())
+d = json.loads(open('input/webqsp.test.entities.with_classes.json').read())
 
 collection = NIFCollection(uri="http://sda.tech/webquestions")
 
@@ -12,13 +12,15 @@ for item in d:
     uid = item['question_id']
     context = collection.add_context(uri="http://sda.tech/webquestions/%s"%uid,
     mention=item['utterance'])
+    beg = 0
     for entity in item['entities']:
         if entity is None:
             continue
-        context.add_phrase(taIdentRef='http://www.wikidata.org/entity/'+entity, beginIndex=0, endIndex=1)
+        context.add_phrase(taIdentRef='http://www.wikidata.org/entity/'+entity, beginIndex=beg, endIndex=beg+1)
+        beg+=1
 
 generated_nif = collection.dumps(format='turtle')
-f = open('webqsp.train.nif','w')
+f = open('webqsp.test.nif','w')
 f.write(generated_nif)
 f.close()
 
